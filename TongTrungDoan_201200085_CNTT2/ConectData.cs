4af0d9eefebdc49c93438fa7233f8b4eec2fd12d
tongdoan <tongdoan2002@gmail.com>
2022-10-28 16:49:02 +0700

    code here

---
+++ TongTrungDoan_201200085_CNTT2/ConectData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TongTrungDoan_201200085_CNTT2
+{
+    internal class ConectData
+    {
+        SqlConnection sqlConnection;
+        public void Connect()
+        {
+            sqlConnection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\MenuObject\\LTTQ\\TongTrungDoan_201200085_CNTT2\\TongTrungDoan_201200085_CNTT2\\Database1.mdf;Integrated Security=True");
+
+            if (sqlConnection.State != ConnectionState.Open)
+            {
+                sqlConnection.Open();
+            }
+
+        }
+        public void closeConnect()
+        {
+
+            if (sqlConnection.State != ConnectionState.Closed)
+            {
+                sqlConnection.Close();
+            }
+        }
+        public DataTable table(string query)
+        {
+            DataTable table = new DataTable();
+            Connect();
+            SqlDataAdapter adapter = new SqlDataAdapter(query, sqlConnection);
+            adapter.Fill(table);
+            closeConnect();
+            table.Dispose();
+            return table;
+        }
+        public void excute(string query)
+        {
+            Connect();
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            sqlCommand.ExecuteNonQuery();
+            closeConnect();
+        }
+    }
+}
